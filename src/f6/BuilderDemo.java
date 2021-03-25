ed3d33d16bb4fd31996ba5588e5e3d28473b6fd0
Даниил <nikolaevdanil2000@mail.ru>
2021-03-25 12:30:34 +0300

    Pr

---
+++ src/f6/BuilderDemo.java
@@ -0,0 +1,9 @@
+package f6;
+
+public class BuilderDemo {
+    public static void main(String args[])
+    {
+        StudentReceiver sr = new StudentReceiver();
+        System.out.println(sr.getStudent());
+    }
+}
